8e9317fe1649182a8153bf9f67823db57b42353a
SamuelWood <samuel.r.wood@okstate.edu>
2019-03-13 21:42:16 -0500

    Revert "REdo teleport and enemy bug fixes"
    This reverts commit db66244a411d204fde95155f282f68222bb7d52d.

--- Assets/Scripts/Zelda.cs
+++ Assets/Scripts/Zelda.cs
@@ -28,8 +28,6 @@ public class Zelda : MonoBehaviour
     private bool cameraLeft;                                 // can the camera move left
     private float camMargin;                                 // used to determine when camera should move (relative to the player)
     private Vector3 totalMove;                               // used to change camera position
-    private bool teleported;
-    private float teleportedTimer;
 
     public Transform groundcheck;
     public Transform groundcheck1;
@@ -38,11 +36,6 @@ public class Zelda : MonoBehaviour
     public Transform groundcheck4;
     public LayerMask backstage;
     public LayerMask stage;
-
-    //Used to calculate teleport distance into lost woods
-    public GameObject teleportTakeOff;
-    public GameObject teleportLanding;
-    private Vector3 teleportDown;
 
     void Start()
     {
@@ -61,10 +54,6 @@ public class Zelda : MonoBehaviour
         Physics2D.IgnoreCollision(zeldaHitbox, swordAttackbox);
         Physics2D.IgnoreCollision(zeldaHitbox, edCol);
         Physics2D.IgnoreCollision(edCol, swordAttackbox);
-
-        teleportedTimer = 0;
-        teleported = false;
-        teleportDown = Vector3.down * (Mathf.Abs(teleportLanding.transform.position.y - teleportTakeOff.transform.position.y));
     }
 
     void Update()
@@ -120,16 +109,6 @@ public class Zelda : MonoBehaviour
             else
             {
                 anim.Play("Idle");
-            }
-
-            if (teleported)
-            {
-                teleportedTimer += Time.deltaTime;
-                if (teleportedTimer >= 1)
-                {
-                    teleported = false;
-                    teleportedTimer = 0;
-                }
             }
         }
 
@@ -248,38 +227,30 @@ public class Zelda : MonoBehaviour
         totalMove = Vector3.zero;
     }
 
+    private bool wrrryyyyyy = true;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("TeleportIn"))
+        if (wrrryyyyyy)
         {
-            if (!teleported)
+            if (other.gameObject.CompareTag("TeleportIn"))
             {
                 laugh.Play();
                 other.gameObject.SetActive(false);
-                //Vector3 teleportVector = Vector3.down * 76f;
-                Vector3 teleportVector = teleportDown;
+                Vector3 teleportVector = Vector3.down * 77.01f;
                 transform.position += teleportVector;
                 camControl.CamMode(true);
                 cameraLeft = true;
                 camControl.Teleport(teleportVector);
-                teleported = true;
             }
-        }
-        else if (other.gameObject.CompareTag("TeleportBack"))
-        {
-            if (!teleported)
+            else if (other.gameObject.CompareTag("TeleportBack"))
             {
                 laugh.Play();
                 Vector3 teleportVector = Vector3.left * 121.6f;
                 transform.position += teleportVector;
                 camControl.Teleport(teleportVector);
-                teleported = true;
             }
-        }
-        else if (other.gameObject.CompareTag("TeleportOut"))
-        {
-            if (!teleported)
+            else if (other.gameObject.CompareTag("TeleportOut"))
             {
                 chime.Play();
                 Vector3 teleportVector = new Vector3(61f, 77.1f, 0f);
@@ -287,12 +258,11 @@ public class Zelda : MonoBehaviour
                 camControl.CamMode(false);
                 cameraLeft = false;
                 camControl.Teleport(teleportVector);
-                teleported = true;
             }
-        }
-        else if (other.gameObject.CompareTag("FreezeCamera"))
-        {
-            camControl.AffixCamera();
+            else if (other.gameObject.CompareTag("FreezeCamera"))
+            {
+                camControl.AffixCamera();
+            }
         }
     }
 }
